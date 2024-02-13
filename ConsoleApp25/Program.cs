c6a78d028a66aaa1115ab11b0f15a9c70de10baa
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-02-13 13:11:48 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,39 @@
+﻿// track
+
+
+// test 
+
+string fileName = "pass.txt";
+
+string password;
+
+if (File.Exists(fileName))
+{
+    password = File.ReadAllText(fileName);
+}
+else
+{
+    File.Create(fileName).Close();
+    password = Registration();
+}
+
+Console.WriteLine("Введите пароль");
+
+if(Console.ReadLine () == password)
+{
+    Console.WriteLine("Доступ  разрешен");
+}
+else
+{
+    Console.WriteLine("Доступ не разрешен");
+}
+
+
+string Registration()
+{
+    Console.WriteLine("регистрация в  системе");
+    Console.WriteLine("введите пароль");
+    string pas = Console.ReadLine();
+    File.WriteAllText(fileName, pas);
+    return pas;
+}
